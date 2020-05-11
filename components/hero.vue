9a13b4aed4eef16e2d5f6fa8646b608118e10b2f
amradelata <amradelata@gmail.com>
2020-05-11 05:42:14 +0200

    chang the hero

--- components/hero.vue
+++ components/hero.vue
@@ -1,32 +1,38 @@
 <template>
-  <section class="hero is-fullheight myHero">
-    <img src="~/assets/img/shap-5.svg" alt="shap-4" class="shap-4" />
-    <img src="~/assets/img/shap-5.svg" alt="shap-5" class="shap-5" />
-    <div class="hero-body">
-      <div class="container">
-        <div class="columns mainhero">
-          <div class="column is-half my-content">
-            <span class="is-size-5 is-bold is-white">Hi,</span>
-            <p class="is-size-4">My name is Amr Adel Ata</p>
-            <p class="is-size-1">Front end developer</p>
-            <div class="tow-images">
-              <img src="~/assets/img/icons/js.png" alt="js" />
-              <img src="~/assets/img/icons/vue.png" alt="vue" />
-            </div>
-            <div class="herobtn">
-              <a
-                href="https://github.com/amradelata/portfolio-vue/blob/master/assets/cv/Amr-adel-ata-frontEndCV.pdf"
-                download
-                target="_blank"
-                class="button downloadCvBtn radio"
-              >Download CV</a>
-              <a class="button downloadCvBtn radio" @click="scrollToMyProjects()">Projects</a>
-            </div>
-          </div>
-          <div class="column is-half hero-emage"></div>
+  <!-- start hero Section -->
+  <section class="hero">
+    <div class="allherocontent">
+      <div class="contents">
+        <span class="is-size-5 is-bold is-white">Hi,</span>
+        <p class="is-size-4">My name is Amr Adel Ata</p>
+        <p class="is-size-1">Front end developer</p>
+        <!--  -->
+        <a
+          href="https://github.com/amradelata/portfolio-vue/blob/master/assets/cv/Amr-adel-ata-frontEndCV.pdf"
+          download
+          target="_blank"
+          class="button downloadCvBtn radio"
+        >Download CV</a>
+        <a class="button radio seeprojects" @click="scrollToMyProjects()">Projects</a>
+        <!--  -->
+        <div class="tow-images">
+          <img src="~/assets/img/icons/js.png" alt="js" />
+          <img src="~/assets/img/icons/vue.png" alt="vue" />
         </div>
       </div>
+      <div class="heroimg">
+        <img src="~/assets/img/logos/amr.jpeg" class />
+      </div>
     </div>
+    <img
+      src="~/assets/img/herosvg/bottom-right-corner.svg"
+      class="top-right-corner scale-in-center"
+    />
+    <img
+      src="~/assets/img/herosvg/top-right-corner.svg"
+      class="bottom-right-corner scale-in-center"
+    />
+    <img src="~/assets/img/herosvg/left.svg" class="left-corner scale-in-center" />
   </section>
 </template>
 
@@ -43,4 +49,7 @@ export default {
 </script>
 
 <style  scoped>
+.herocontainer {
+  padding: 0 10%;
+}
 </style>
